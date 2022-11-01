0e25b0ece3096d522c99b4691f469c5fe2e0a59f
José Flávio <jose.flavio@sou.unifal-mg.edu.br>
2022-11-01 13:24:50 -0300

    primeira versao sprn

--- codigos/main.py
+++ codigos/main.py
@@ -5,22 +5,19 @@ from npe import NPE
 def main():
     g = Graph(oriented = True)
 
-    g.setV(16)
+    g.setV(13)
     g.addEdge(0, 1)
     g.addEdge(0, 2)
-    g.addEdge(0, 3)
+    g.addEdge(1, 8)
+    g.addEdge(1, 7)
+    g.addEdge(2, 3)
     g.addEdge(2, 4)
-    g.addEdge(2, 8)
-    g.addEdge(4, 5)
-    g.addEdge(5, 6)
-    g.addEdge(5, 7)
-    g.addEdge(8, 9)
-    g.addEdge(9, 10)
+    g.addEdge(3, 5)
+    g.addEdge(3, 10)
+    g.addEdge(4, 6)
+    g.addEdge(6, 9)
+    g.addEdge(6, 12)
     g.addEdge(10, 11)
-    g.addEdge(10, 12)
-    g.addEdge(9, 13)
-    g.addEdge(8, 14)
-    g.addEdge(14, 15)
 
     #print(g)
 
@@ -32,5 +29,10 @@ def main():
     npe = NPE(g)
 
     print(npe.decode())
+
+    (N, D) = npe.SPRN(10, 8)
+
+    print("node: " + str(N))
+    print("depth: " + str(D))
 
 main()
--- codigos/npe.py
+++ codigos/npe.py
@@ -22,3 +22,35 @@ class NPE:
             lookup[d[i]] = n[i]
         
         return edges
+    
+    def SPRN(self, p, a):
+        n = self.npe["node"]
+        d = self.npe["depth"]
+
+        ia = n.index(a)
+        ip = n.index(p)
+        il = ip + 1
+
+        while il < len(n) and d[il] > d[ip]:
+            il = il + 1
+        
+        rp = range(ip, il)
+
+        N = []
+        D = []
+
+        for i in range(0, ia + 1):
+            if i not in rp:
+                N.append(n[i])
+                D.append(d[i])
+        
+        for i in rp:
+            N.append(n[i])
+            D.append(d[i] - d[ip] + d[ia] + 1)
+
+        for i in range(ia + 1, len(n)):
+            if i not in rp:
+                N.append(n[i])
+                D.append(d[i])
+
+        return (N, D)
