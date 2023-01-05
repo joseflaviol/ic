9ba5d938cb23bb77054e4c467121ffd6ac501ba1
José Flávio <joseflaviol119@gmail.com>
2023-01-05 20:54:42 -0300

    TBRN

--- codigos/main.py
+++ codigos/main.py
@@ -1,25 +1,28 @@
 from graph import Graph
-from edge_weighted_graph import EdgeWeightedGraph
-from steiner_tree import SteinerTree
 from dfs import DepthFirstSearch
 from npe import NPE
 
 def main():
-    g = Graph(oriented = True)
+    g = Graph(oriented = False)
 
-    g.setV(13)
+    g.setV(17)
+
     g.addEdge(0, 1)
+    g.addEdge(1, 3)
+    g.addEdge(3, 16)
     g.addEdge(0, 2)
-    g.addEdge(1, 8)
-    g.addEdge(1, 7)
-    g.addEdge(2, 3)
     g.addEdge(2, 4)
-    g.addEdge(3, 5)
-    g.addEdge(3, 10)
-    g.addEdge(4, 6)
+    g.addEdge(4, 5)
+    g.addEdge(5, 6)
     g.addEdge(6, 9)
-    g.addEdge(6, 12)
-    g.addEdge(10, 11)
+    g.addEdge(9, 10)
+    g.addEdge(5, 7)
+    g.addEdge(7, 8)
+    g.addEdge(4, 13)
+    g.addEdge(4, 14)
+    g.addEdge(4, 15)
+    g.addEdge(2, 11)
+    g.addEdge(0, 12)
 
     #print(g)
 
@@ -32,7 +35,12 @@ def main():
 
     print(npe.decode())
 
-    (N, D) = npe.SPRN(10, 8)
+    (N, D) = npe.SPRN(6, 3)
+
+    print("node: " + str(N))
+    print("depth: " + str(D))
+
+    (N, D) = npe.TBRN2(5, 10, 3)
 
     print("node: " + str(N))
     print("depth: " + str(D))
--- codigos/npe.py
+++ codigos/npe.py
@@ -54,3 +54,82 @@ class NPE:
                 D.append(d[i])
 
         return (N, D)
+    
+    '''
+        TO DO:
+            -   aleatorizar a escolha do vertice p (checar artigo)
+            -   aleatorizar a escolha da aresta (r, a) (checar artigo)
+    '''
+
+    def TBRN2(self, p, r, a):
+        n = self.npe["node"]
+        d = self.npe["depth"]
+
+        ip = n.index(p)
+        ir = n.index(r)
+        ia = n.index(a)
+
+        ep = ip + 1
+        rp = [p] 
+
+        while ep < len(n) and d[ep] > d[ip]:
+            rp.append(n[ep])
+            ep += 1
+
+        er = ir + 1
+        rr = [r] 
+
+        while er < len(n) and d[er] > d[ir]:
+            rr.append(n[er])
+            er += 1
+
+        Ntmp = []
+        Dtmp = []
+
+        for i in range(ir, er):
+            Ntmp.append(n[i])
+            Dtmp.append( d[i] - d[ir] + d[ia] + 1 )
+
+        N = []
+        D = []
+
+        for i in range(0, ia + 1):
+            if i < ip or i >= ep:
+                N.append(n[i])
+                D.append(d[i])
+
+        ix = ir 
+        fx = None 
+
+        while ix != ip:
+
+            for i in range(ix - 1, ip - 1, -1):
+                if d[i] == d[ix] - 1:
+                    fx = i 
+                    break 
+
+            ex = fx + 1
+            
+            while ex < len(n) and d[ex] > d[fx]:
+                ex += 1    
+
+            aux_ix = Ntmp.index(n[ix])
+
+            for i in range(fx, ex):
+                if n[i] not in rr:
+                    rr.append(n[i])
+                    Ntmp.append(n[i])
+                    Dtmp.append(d[i] - d[fx] + Dtmp[aux_ix] + 1)
+            
+            ix = fx
+
+        N += Ntmp
+        D += Dtmp
+
+        for i in range(ia + 1, len(n)):
+            if i < ip or i >= ep:
+                N.append(n[i])
+                D.append(d[i])
+
+        return (N, D)
+    
